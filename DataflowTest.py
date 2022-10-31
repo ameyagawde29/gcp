3e680768dae26534ad4238ba9226c84aa65024e9
ameyagawde29 <94888739+ameyagawde29@users.noreply.github.com>
2022-10-31 14:43:00 -0400

    Update DataflowTest.py

--- DataflowTest.py
+++ DataflowTest.py
@@ -37,9 +37,9 @@ class ReadFile(beam.DoFn):
     def process(self, something):
         clear_data = []
         with open(self.input_path) as fin:
-            for line in fin:
-                data = json.loads(line)
-                product = data.get('product')
+            ss=fin.read()
+            data = json.loads(ss)
+            product = data.get('product')
         
         
         
